a0035a1fa4bb1bd23e51a96d029d781203074200
wurining <wurining@gmail.com>
2023-01-23 05:00:34 +0000

    v1.5 - add baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,8 +14,13 @@ export default defineNuxtConfig({
     app: {
         head: {
             meta: [
-                // <meta name="viewport" content="width=device-width, initial-scale=1">
-                { name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover' }
+                { name: 'title', content: 'Matcha Cat🐱' },
+                { name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover' },
+                { property: 'og:type', content: 'website' },
+                { property: 'og:title', content: 'Matcha Cat🐱' },
+                { property: 'og:description', content: '菜单' },
+                { property: 'og:image', content: 'http://matchacat.wurining.com/logo.jpeg' },
+                { property: 'og:url', content: 'http://matchacat.wurining.com/' },
             ],
             "link": [],
             "style": [],
